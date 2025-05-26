79abd7e1a99b2ea7de1396a7a9d63f285d1da6cb
Abderlal <alalthoum46528-student@enaco.fr>
2025-05-26 16:45:31 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
-
-// import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "@/contexts/AuthContext";
-import ProtectedRoute from "@/components/ProtectedRoute";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import ClientDashboard from "./pages/ClientDashboard";
-import TechnicianSearch from "./pages/TechnicianSearch";
-import TechnicianResults from "./pages/TechnicianResults";
-import TechnicianProfile from "./pages/TechnicianProfile";
-import TechnicianDashboard from "./pages/TechnicianDashboard";
-import TechnicianAvailability from "./pages/TechnicianAvailability";
-import Home from "@/pages/Home";
-
-
-const queryClient = new QueryClient();
+import React from 'react'
+import './index.css'
 
 function App() {
   return (
